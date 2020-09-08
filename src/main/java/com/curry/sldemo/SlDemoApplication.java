908164a7d91aefbd4568a35fd6af177b47e93b94
Christian Curry <christian.curry17@gmail.com>
2020-09-08 18:45:08 -0400

    add working spring boot template

---
+++ src/main/java/com/curry/sldemo/SlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.curry.sldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curry/sldemo/SlDemoApplicationTest.java
@@ -0,0 +1,13 @@
+package com.curry.sldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlDemoApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
